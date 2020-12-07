2d8af44a414b13da229bc130d0aa915d80958b1e
jsksru <mail@jsks.ru>
2020-12-07 19:27:09 +0500

    add mock data to filter

--- src/app/modules/widgets/filter/filter.component.ts
+++ src/app/modules/widgets/filter/filter.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { MultiSelectItems } from '../../../types/controls/multi-select.types';
+import { RadioListItems } from '../../../types/controls/radio-list.types';
 
 @Component({
   selector: 'widget-filter',
   templateUrl: './filter.component.html',
   styleUrls: ['./filter.component.scss']
 })
-export class FilterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class FilterComponent {
+  filter: MultiSelectItems = [
+    {
+      value: '#000',
+      text: 'Black'
+    },
+    {
+      value: 'rgb(255,0,0)',
+      text: 'Red'
+    },
+    {
+      value: 'fam',
+      text: 'Family'
+    }
+  ];
+  radios: RadioListItems = [
+    {
+      value: '#000',
+      text: 'Black'
+    },
+    {
+      value: 'rgb(255,0,0)',
+      text: 'Red'
+    },
+    {
+      value: 'fam',
+      text: 'Family'
+    }
+  ];
 }
