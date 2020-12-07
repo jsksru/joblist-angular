c235bd1c5a4f95dead47bddbb8df2cc1f639dd4b
jsksru <mail@jsks.ru>
2020-12-07 19:25:09 +0500

    checkbox logic

--- src/app/modules/controls/checkbox/checkbox.component.ts
+++ src/app/modules/controls/checkbox/checkbox.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'controls-checkbox',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./checkbox.component.scss']
 })
 export class CheckBoxComponent {
+  @Input() checked: boolean;
+  @Output() changeEmitter: EventEmitter<boolean> = new EventEmitter();
 
+  changeHandler(e) {
+    this.changeEmitter.emit(e.target.checked);
+  }
 }
