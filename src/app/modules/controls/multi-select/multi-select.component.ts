c144b6d4243c9d483a4f8debb5121bd770e0bf90
jsksru <mail@jsks.ru>
2020-12-07 19:26:18 +0500

    multiselect logic and types

--- src/app/modules/controls/multi-select/multi-select.component.ts
+++ src/app/modules/controls/multi-select/multi-select.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { MultiSelectItems } from '../../../types/controls/multi-select.types';
 
 @Component({
   selector: 'controls-multi-select',
@@ -6,13 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./multi-select.component.scss']
 })
 export class MultiSelectComponent {
+  @Input() items: MultiSelectItems;
+  @Output() selectEmitter: EventEmitter<string[]> = new EventEmitter();
 
-  inputValue: string = '';
+  inputValue = '';
   isEmpty = true;
+  isOpen = false;
+
+  filteredItems: string[] = [];
 
   onInput(e) {
     this.inputValue = e.target.value;
     this.isEmpty = e.target.value.length > 0 ? false : true;
+  }
+
+  filterItems(value: string, status: boolean) {
+    if (status) {
+      this.filteredItems.push(value);
+    } else {
+      this.filteredItems = this.filteredItems.filter(i => i !== value);
+    }
+    this.selectEmitter.emit(this.filteredItems);
+  }
+
+  toggleHandler() {
+    this.isOpen = !this.isOpen;
   }
 
 }
