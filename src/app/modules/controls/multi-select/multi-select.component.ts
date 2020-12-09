3b95b80073a2cf901c139cbd831eb2b5bf6a5ff2
jsksru <mail@jsks.ru>
2020-12-09 17:42:29 +0500

    multiselect logic

--- src/app/modules/controls/multi-select/multi-select.component.ts
+++ src/app/modules/controls/multi-select/multi-select.component.ts
@@ -1,13 +1,17 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { MultiSelectItems } from '../../../types/controls/multi-select.types';
+
+
 
 @Component({
   selector: 'controls-multi-select',
   templateUrl: './multi-select.component.html',
   styleUrls: ['./multi-select.component.scss']
 })
-export class MultiSelectComponent {
+export class MultiSelectComponent implements OnInit{
   @Input() items: MultiSelectItems;
+  @Input() title: string;
+  @Input() values: string[];
   @Output() selectEmitter: EventEmitter<string[]> = new EventEmitter();
 
   inputValue = '';
@@ -16,9 +20,23 @@ export class MultiSelectComponent {
 
   filteredItems: string[] = [];
 
-  onInput(e) {
-    this.inputValue = e.target.value;
-    this.isEmpty = e.target.value.length > 0 ? false : true;
+  ngOnInit() {
+    this.filteredItems = [...this.values];
+    this.printTotal();
+  }
+
+  isChecked(value: string): boolean {
+    return this.values.some(i => i === value);
+  }
+
+  printTotal() {
+    if (this.filteredItems.length > 0) {
+      this.isEmpty = false;
+      this.inputValue = 'Выбраны ' + this.filteredItems.length;
+    } else {
+      this.isEmpty = true;
+      this.inputValue = '';
+    }
   }
 
   filterItems(value: string, status: boolean) {
@@ -27,6 +45,7 @@ export class MultiSelectComponent {
     } else {
       this.filteredItems = this.filteredItems.filter(i => i !== value);
     }
+    this.printTotal();
     this.selectEmitter.emit(this.filteredItems);
   }
 
