170c9e0e815003ea2736b9fa985ff90fd78a8ef9
jsksru <mail@jsks.ru>
2020-12-13 08:39:32 +0500

    types

---
+++ src/app/modules/pages/main/main.types.ts
@@ -0,0 +1,13 @@
+export type Ship = {
+  id: string;
+  name: string;
+  type: string;
+  port: string;
+};
+
+export type ShipResult = {
+  id: string;
+  name: string;
+  type: string;
+  home_port: string;
+};
